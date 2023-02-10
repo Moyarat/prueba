0fcde441df52e05eea31a388d48f4d657a560b63
Moyarat <cristianzequ@unisabana.edu.co>
2023-02-09 20:12:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Despues de 10 minutos al fin xd <code>xd</code> Aprendele mas
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vamos por ese primer sueldito de 2 millones o mas
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
